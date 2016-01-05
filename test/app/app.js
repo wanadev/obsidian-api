900b25e7d532a7bc7feaa0d0665652395d61312c
Fabien LOISON <flo@flogisoft.com>
2016-01-05 16:55:08 +0100

    Remote method call / response

--- test/app/app.js
+++ test/app/app.js
@@ -1,13 +1,55 @@
 "use strict";
 
+var Q = require("q");
 var WanadevProjectApi = require("../../lib/WanadevProjectApi.js");
 
 var Api = WanadevProjectApi.$extend({
 
     method1: function() {
         "@api";
-    }
+    },
 
+    addSyncOk: function(a, b) {
+        "@api";
+        return a + b;
+    },
+
+    addSyncError: function(a, b) {
+        "@api";
+        throw new Error("TestError");
+    },
+
+    addAsyncCallbackOk: function(a, b, callback) {
+        "@api";
+        setTimeout(function() {
+            callback(null, a + b);
+        }, 1);
+    },
+
+    addAsyncCallbackError: function(a, b, callback) {
+        "@api";
+        setTimeout(function() {
+            callback(new Error("TestError"));
+        }, 1);
+    },
+
+    addAsyncPromiseOk: function(a, b) {
+        "@api";
+        return Q.Promise(function(resolve, reject) {
+            setTimeout(function() {
+                resolve(a + b);
+            }, 1);
+        });
+    },
+
+    addAsyncPromiseError: function(a, b) {
+        "@api";
+        return Q.Promise(function(resolve, reject) {
+            setTimeout(function() {
+                reject(new Error("TestError"));
+            }, 1);
+        });
+    }
 });
 
 var api = new Api();
--- test/integration.js
+++ test/integration.js
@@ -27,4 +27,84 @@ describe("Integration", function() {
         });
     });
 
+    it("can call a remote method that returns a result", function(done) {
+        var app = this.app;
+        this.app.on("ready", function() {
+            app.addSyncOk(1, 2, function(error, result) {
+                expect(error).to.be(null);
+                expect(result).to.be(3);
+                done();
+            });
+        });
+    });
+
+    it("can call a remote method that returns an error", function(done) {
+        var app = this.app;
+        this.app.on("ready", function() {
+            app.addSyncError(1, 2, function(error, result) {
+                expect(error).to.be.an(Error);
+                expect(error.toString()).to.match(/TestError/);
+                expect(result).to.be(undefined);
+                done();
+            });
+        });
+    });
+
+    it("can call remote methods that return a result (using promises)", function() {
+        var app = this.app;
+        return app.addSyncOk(1, 2)
+            .then(function(result) {
+                expect(result).to.equal(3);
+            })
+
+            .then(function() {
+                return app.addAsyncCallbackOk(1, 2);
+            })
+            .then(function(result) {
+                expect(result).to.equal(3);
+            })
+
+            .then(function() {
+                return app.addAsyncPromiseOk(1, 2);
+            })
+            .then(function(result) {
+                expect(result).to.equal(3);
+            });
+    });
+
+    it("can call remote methods that return an error (using promises)", function() {
+        var app = this.app;
+        return app.addSyncError(1, 2)
+            .then(function() {
+                throw new Error("ShouldNotSucceed");
+            })
+            .catch(function(error) {
+                expect(error).to.be.an(Error);
+                expect(error.toString()).to.match(/TestError/);
+            })
+
+            .then(function() {
+                return app.addAsyncCallbackError(1, 2);
+            })
+            .then(function() {
+                throw new Error("ShouldNotSucceed");
+            })
+            .catch(function(error) {
+                expect(error).to.be.an(Error);
+                expect(error.toString()).to.match(/TestError/);
+            })
+
+
+            .then(function() {
+                return app.addAsyncPromiseError(1, 2);
+            })
+            .then(function() {
+                throw new Error("ShouldNotSucceed");
+            })
+            .catch(function(error) {
+                expect(error).to.be.an(Error);
+                expect(error.toString()).to.match(/TestError/);
+            });
+    });
+
 });
